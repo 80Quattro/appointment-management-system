898324ab5a6d521d5e623b516650fba9f97d5884
80Quattro <muzyczek2@gmail.com>
2022-11-12 19:01:04 +0100

    Add basic validation in AppointmentController:create

--- src/Controller/AppointmentController.php
+++ src/Controller/AppointmentController.php
@@ -9,14 +9,17 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 #[Route('/api/appointment', name: 'api_appointment')]
 class AppointmentController extends AbstractController
 {
     private $entityManager;
+    private $validator;
 
-    public function __construct(ManagerRegistry $doctrine) {
+    public function __construct(ManagerRegistry $doctrine, ValidatorInterface $validator) {
         $this->entityManager = $doctrine->getManager();
+        $this->validator = $validator;
     }
 
     #[Route('/create', name: 'api_appointment_create', methods: "POST")]
@@ -27,6 +30,15 @@ class AppointmentController extends AbstractController
         $appointment = new Appointment();
         $appointment->setDate( \DateTime::createFromFormat('d/m/Y H:i', $date) )
             ->setStatus('RESERVED');
+
+        $errors = $this->validator->validate($appointment);
+        
+        if (count($errors) > 0) {
+
+            $errorsString = (string) $errors;
+    
+            return new Response($errorsString);
+        }
 
         $this->entityManager->persist($appointment);
         $this->entityManager->flush();
