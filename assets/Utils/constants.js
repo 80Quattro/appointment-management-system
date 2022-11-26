011d10ae35795d135520af23db0c18bcc46901c9
80Quattro <muzyczek2@gmail.com>
2022-11-26 16:50:10 +0100

    Add basic react routing

---
+++ assets/Utils/constants.js
@@ -0,0 +1,12 @@
+const API_URL = 'http://localhost:8000'
+export const API_ROUTES = {
+    SIGN_UP: `${API_URL}/api/register`,
+    SIGN_IN: `${API_URL}/api/login_check`,
+}
+
+export const APP_ROUTES = {
+    HOME: '/',
+    SIGN_UP: '/signup',
+    SIGN_IN: '/signin',
+    DASHBOARD: '/dashboard',
+}
--- assets/app.js
+++ assets/app.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from "react-dom";
+import router from './Components/Router'
+import { RouterProvider } from "react-router-dom";
 
 const App = () => {
     return (
-        <div>
-            Hello!
-        </div> 
+        <React.StrictMode>
+            <RouterProvider router={router} />
+        </React.StrictMode>
     );
 }
  
